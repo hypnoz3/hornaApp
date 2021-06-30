963c11a0f6e67ab6a282a11eda414f5364dcce54
hypnoz3 <74300099+hypnoz3@users.noreply.github.com>
2021-06-30 23:00:18 +0300

    updating raid-history and other stuff

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,12 @@
-function changeBanner(banner) {
-    alert("asd");
-    //$('#expansion-banner').attr('src', banner);
-    //document.querySelector('#expansion-banner').src = banner;
-}
+// function changeBanner(banner) {
+//     alert("asd");
+//     $('#expansion-banner').attr('src', banner);
+//     document.querySelector('#expansion-banner').src = banner;
+// }
 
 
 function changeBanner(banner) {
-    //$('#expansion-banner').attr('src', banner);
+    $('#expansion-banner').attr('src', banner);
     document.querySelector('#expansion-banner').src = banner;
 }
 
@@ -25,3 +25,18 @@ buttons.forEach((btn) => {
         </span>`
     })
   })
+
+  // Expansion filter //
+  const wodButton = getElementById('#wod_box');
+  const legionButton = getElementById('#legion_box')
+  let wod = expansion.findById('60dcb8d6de0ebf57f408a01e').then(console.log(wod));
+  const Expansion = require('../models/expansion');
+   function legion(Expansion) {
+      expansion.findById('60dc58717404dd38d85c4307');
+      console.log(Expansion)
+  }
+
+
+
+
+  
