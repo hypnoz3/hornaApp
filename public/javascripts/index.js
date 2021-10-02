ac3808343c71765716a11ea432a67fa4af343410
hypnoz3 <74300099+hypnoz3@users.noreply.github.com>
2021-10-02 15:33:52 +0300

    cleanup

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,33 +1,15 @@
+const buttonz = document.querySelectorAll('.ripple')
 
-const buttons = document.querySelectorAll('.ripple')
-
-buttons.forEach((btn) => {
-    const btnText = btn.innerHTML
-    btn.addEventListener('click', (e) => {
-      btn.innerHTML = 
-        `${btnText}
+buttonz.forEach((btn) => {
+  const btnText = btn.innerHTML
+  btn.addEventListener('click', (e) => {
+    btn.innerHTML =
+      `${btnText}
         <span
             class="circle"
             style="left:${e.offsetX}px; top:${e.offsetY}px">
         </span>`
-    })
   })
-
- 
-const progress = 'https://raider.io/api/v1/guilds/profile?region=eu&realm=Twisting-nether&name=Horna&fields=raid_progression';
-const progressBOX_nathria = document.getElementById('progress_nathria');
-
-async function getProgress(url) {
-  const res = await fetch(url);
-  const data = await res.json();
-  const { raid_progression} = data
-  
-  console.log(raid_progression['castle-nathria'].summary)
-}
-
-
-getProgress(progress)
-
-
+})
 
 
